3476416034b6bf32194617670f47e67b1bb355a3
vorujack <vorujack@gmail.com>
2016-01-25 17:14:26 +0330

    create setup begin to upload to pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from distutils.core import setup
 setup(
     name="decorate_url",
-    version="1.0",
+    version="1.1",
     packages=find_packages(),
     author="vorujack",
     author_email="info@jaybird.ir",
     description="add wrapping whole url in django urls",
     license="PSF",
     url='https://github.com/vorujack/decorate_url',
-    download_url='https://github.com/vorujack/decorate_url/tarball/1.0',
+    download_url='https://github.com/vorujack/decorate_url/tarball/1.1',
     keywords="django url_wrap",
 )
